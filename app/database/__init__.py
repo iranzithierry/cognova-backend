f6efa2fce4a21871964e1580fa5b5a6ed0c6c988
bendeez <blaziken@MSI>
2024-10-22 19:08:17 -0500

    keeping things simple

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,3 +1,3 @@
 from app.database.repository import JoinExpression, DatabaseQueryOrder, Repository
-from app.database.models import CognovaEntity, KV
+from app.database.models import CognovaEntity
 from app.database.dependencies import get_db
