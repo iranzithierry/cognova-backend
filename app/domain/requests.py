5d8ffd16fcdb8a981abc37bc9523f2e2d5e08ef5
Iranzi Thierry <115815954+iranzithierry@users.noreply.github.com>
2024-11-20 17:07:18 +0200

    Update requests.py

--- app/domain/requests.py
+++ app/domain/requests.py
@@ -3,6 +3,6 @@ from pydantic import BaseModel
 
 class ChatRequest(BaseModel):
     prompt: str
-    wa_phone_number: Optional[str]
-    wa_profile_picture: Optional[str]
-    wa_profile_name: Optional[str]
+    wa_phone_number: Optional[str] = None
+    wa_profile_picture: Optional[str] = None
+    wa_profile_name: Optional[str] = None
