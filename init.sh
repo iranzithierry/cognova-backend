5b21bcd4a0d8193fc863f10cc2bc7faab1a83432
Troy <iranzithierry12@gmail.com>
2024-12-06 19:24:10 +0200

    adding automation workflow

--- init.sh
+++
@@ -1,8 +0,0 @@
-prisma generate
-echo "Prisma Generated"
-
-sed -i.bak "s/'models\./'/" ./venv/lib/python3.*/site-packages/prisma/models.py
-echo "Prisma Models Updated"
-
-chmod +x ./bin/scraper
-echo "Scraper Got Execution Permission"
